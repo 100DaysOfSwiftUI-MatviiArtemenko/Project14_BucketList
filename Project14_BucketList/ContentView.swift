69e0b74dfd834e3abdcacd18d394a97b611914e8
Matvii Artemenko <100190098+MatveyArtemenko@users.noreply.github.com>
2022-09-10 15:45:12 +0200

    Initial Commit

---
+++ Project14_BucketList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project14_BucketList
+//
+//  Created by admin on 10.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project14_BucketList/Project14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project14_BucketListApp.swift
+//  Project14_BucketList
+//
+//  Created by admin on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
